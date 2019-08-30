9dad17f7a16766670eef7087910e04b300fbc30e
pokoyo <zxdddf@gmail.com>
2019-08-29 22:22:39 -0600

    primer 1

---
+++ primer.js
@@ -0,0 +1,11 @@
+function Person(age) {
+    this.age = age;
+    this.growOld = function () {
+        this.age++;
+    };
+}
+var person = new Person(1);
+setTimeout(person.growOld, 1000);
+setTimeout(function () {
+    console.log(person.age);
+}, 2000);
