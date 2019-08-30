d8cad8aa8d486a01762903ae45c1199b088b92cc
pokoyo <zxdddf@gmail.com>
2019-08-29 22:35:48 -0600

    tercer 3

--- tercer.js
+++ tercer.js
@@ -13,3 +13,14 @@ Promise.resolve(123)
 })["catch"](function (err) {
     console.log(err.message);
 });
+Promise.reject(new Error("queapasado como dice el auronplay"))
+    .then(function (res) {
+    console.log(res);
+    return 456;
+})["catch"](function (err) {
+    console.log(err.message);
+    return Promise.resolve(123);
+})
+    .then(function (res) {
+    console.log(res);
+});
--- tercer.ts
+++ tercer.ts
@@ -14,3 +14,15 @@ Promise.resolve(123)
   .catch(err => {
     console.log(err.message);
   });
+Promise.reject(new Error("queapasado como dice el auronplay"))
+  .then(res => {
+    console.log(res); 
+    return 456;
+  })
+  .catch(err => {
+    console.log(err.message);
+    return Promise.resolve(123);
+  })
+  .then(res => {
+    console.log(res);
+  });
