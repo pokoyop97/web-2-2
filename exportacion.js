8f13059bb684bba55dba8ac7316570801646d98b
pokoyo <zxdddf@gmail.com>
2019-08-31 19:55:59 -0600

    exportacion 2

---
+++ exportacion.js
@@ -0,0 +1,10 @@
+var multiplicar = function(x) {
+  return x * 3;
+};
+var sumar = function(x) {
+  return x + 2;
+};
+exports.multiplicar = multiplicar;
+exports.sumar = sumar;
+var saludo = "Hola mundo";
+module.exports = saludo;
---
+++ main2.js
@@ -0,0 +1,4 @@
+var importacion = require("./exportacion");
+console.log("Este es el resultado de la importación " + importacion);
+var tipo = typeof importacion;
+console.log("Este es el tipo del contenido importado " + tipo);
