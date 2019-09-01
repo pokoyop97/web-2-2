317fc8352005985c3e94255c7f81e293735c2702
pokoyo <zxdddf@gmail.com>
2019-08-31 19:45:21 -0600

    exports 1

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var calculos_js_1 = require("./calculos.js");
+console.log(calculos_js_1.sumar(2, 3));
+console.log(calculos_js_1.restar(4, 3));
---
+++ main.ts
@@ -0,0 +1,3 @@
+import { sumar, restar } from './calculos.js';
+console.log(sumar(2, 3));
+console.log(restar(4, 3));
